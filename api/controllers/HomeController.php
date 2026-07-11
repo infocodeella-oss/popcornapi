6251f27e013af2f0a62158dc3b774cf6acd7c665
EMAD OTHMAN <info.codeella@gmail.com>
2026-07-11 20:45:32 +0200

    Home

--- api/controllers/HomeController.php
+++ api/controllers/HomeController.php
@@ -6,48 +6,66 @@ class HomeController
 {
     public function index(): void
     {
+        // $response = [
+        //     'latest_movies' => Supabase::table('movies')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data'],
+
+        //     'latest_series' => Supabase::table('series')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data'],
+
+        //     'latest_dramacafe' => Supabase::table('dramacafe')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data'],
+
+        //     'latest_turkish_series' => Supabase::table('turkish_series')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data'],
+
+        //     'latest_arabic_movies' => Supabase::table('arabic_movies')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data'],
+
+        //     'latest_cafe_series' => Supabase::table('cafe_series')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data'],
+
+        //     'latest_plays' => Supabase::table('plays')->all([
+        //         'select' => '*',
+        //         'order'  => 'id.desc',
+        //         'limit'  => 10
+        //     ])['data']
+        // ];
+
+
+        $latest_added = [];
+        $latest_added[] = Supabase::table('movies')->all([
+            'select' => '*',
+            'order'  => 'id.desc',
+            'limit'  => 2
+        ])['data'];
+
+        $latest_added[] = Supabase::table('series')->all([
+            'select' => '*',
+            'order'  => 'id.desc',
+            'limit'  => 2
+        ])['data'];
+
         $response = [
-            'latest_movies' => Supabase::table('movies')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data'],
-
-            'latest_series' => Supabase::table('series')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data'],
-
-            'latest_dramacafe' => Supabase::table('dramacafe')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data'],
-
-            'latest_turkish_series' => Supabase::table('turkish_series')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data'],
-
-            'latest_arabic_movies' => Supabase::table('arabic_movies')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data'],
-
-            'latest_cafe_series' => Supabase::table('cafe_series')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data'],
-
-            'latest_plays' => Supabase::table('plays')->all([
-                'select' => '*',
-                'order'  => 'id.desc',
-                'limit'  => 10
-            ])['data']
+            'latest_added' => $latest_added
         ];
 
         Response::success($response);
