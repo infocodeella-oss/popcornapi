afd3ebc7be904d9c257337db26caf5e06dc0e07f
EMAD OTHMAN <info.codeella@gmail.com>
2026-07-12 18:25:48 +0200

    Increase Ramadan Series From 20 To 100

--- api/controllers/RamadanController.php
+++ api/controllers/RamadanController.php
@@ -6,12 +6,11 @@ class RamadanController
 {
     public function index(): void
     {
-
         $response = [
             Supabase::table('v_ramadan_first_episodes')->all([
                 'select'  => '*',
                 'section' => 'eq.' . rawurlencode('رمضان 2026'),
-                'limit'   => 20
+                'limit'   => 100
             ])['data']
         ];
 
